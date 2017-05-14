d7cd9f797b0d570a254f61d39e852873736c5e9f
¨Goodness <gtkbrain@gmail.com>
2017-05-14 19:16:53 +0100

    Sign Up works Fine

--- app/src/main/java/me/goodnesskayode/stand/SignIn.java
+++ app/src/main/java/me/goodnesskayode/stand/SignIn.java
@@ -3,14 +3,20 @@ package me.goodnesskayode.stand;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignIn extends AppCompatActivity {
 
     TextView rSignUp;
     Button rMain;
+    private EditText iEmail;
+    private EditText iPassword;
+    private Button mSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,32 @@ public class SignIn extends AppCompatActivity {
                 startActivity(main);
             }
         });
+
+        iEmail = (EditText) findViewById(R.id.email);
+        iPassword = (EditText) findViewById(R.id.password);
+        mSignIn = (Button) findViewById(R.id.signin_button);
+
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = iEmail.getText().toString().trim();
+                String password = iPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(getApplicationContext(),"Email box is empty", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(getApplicationContext(),"Password box is empty", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+            }
+        });
+
+
     }
 
 }
--- app/src/main/java/me/goodnesskayode/stand/Signup.java
+++ app/src/main/java/me/goodnesskayode/stand/Signup.java
@@ -1,15 +1,32 @@
 package me.goodnesskayode.stand;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Signup extends AppCompatActivity {
 
     TextView rSignIn;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    private EditText iEmail;
+    private EditText iPassword;
+    private Button mSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +34,6 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         rSignIn = (TextView) findViewById(R.id.sign_in_button);
-
         rSignIn.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -26,5 +42,72 @@ public class Signup extends AppCompatActivity {
                 startActivity(signIn);
             }
         });
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        iEmail = (EditText) findViewById(R.id.email);
+        iPassword = (EditText) findViewById(R.id.password);
+        mSignUp = (Button) findViewById(R.id.register_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        mSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = iEmail.getText().toString().trim();
+                String password = iPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(getApplicationContext(),"Email is not valid",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(getApplicationContext(),"Password box is empty",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(password.length() < 6)
+                {
+                    Toast.makeText(getApplicationContext(),"Password too short",Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                //create user
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Signup.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(Signup.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(Signup.this, "Authentication failed." +task.getException(),
+                                            Toast.LENGTH_LONG).show();
+                                } else {
+                                    startActivity(new Intent(Signup.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
 }
