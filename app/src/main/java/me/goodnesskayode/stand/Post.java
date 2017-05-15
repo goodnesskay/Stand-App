2af0ed96c91065b33ab87b89f9b5be8d39ee0d5b
¨Goodness <gtkbrain@gmail.com>
2017-05-15 03:17:04 +0100

    Started work on the Posts

---
+++ app/src/main/java/me/goodnesskayode/stand/Post.java
@@ -0,0 +1,13 @@
+package me.goodnesskayode.stand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
